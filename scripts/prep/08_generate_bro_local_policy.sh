d9ac59f382e623ad229a838a41f290fe29e092ff
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-12-23 18:25:28 +0800

    update broctl.cfg for gold

--- scripts/prep/08_generate_bro_local_policy.sh
+++ scripts/prep/08_generate_bro_local_policy.sh
@@ -3,3 +3,7 @@
 if [[ -f /usr/local/bro/share/bro/policy/protocols/conn/mac-logging.bro ]]; then
     sed -i 's=#@load policy/protocols/conn/mac-logging=@load policy/protocols/conn/mac-logging=' /home/pi/firewalla/etc/local.bro # uncomment mac-logging if file exists
 fi
+
+if [[ -f /usr/local/bro/share/bro/base/frameworks/communication/main.bro ]]; then
+    sed -i 's/#redef Communication::listen_interface = 127.0.0.1;/redef Communication::listen_interface = 127.0.0.1;/' /home/pi/firewalla/etc/local.bro # uncomment Communication if file exists
+fi
